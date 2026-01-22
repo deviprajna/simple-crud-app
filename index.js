e7d8d1b7c9f769a204a3de22162ed3b96efb733f
Devi prajna <deviparamita93@gmail.com>
2026-01-22 21:26:01 +0700

    route POST

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use = (express.json());
 app.post('/food', async (req, res)=>{
     try{
         const Foodie = await Foodie.create(req.body);
+        res.status(200).json(Foodie);
     } catch (error){
         res.status(500).json({message: error.message});
     }
