6cbdaa4d0d4a05b546861e3769a748bad275f0e4
Devi prajna <deviparamita93@gmail.com>
2026-01-22 21:24:46 +0700

    route POST

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 // import library express 
-const express = require ('express')
-
+const express = require ('express'); 
 // import mongoose (for connect to MangoDB)
 const mongoose = require('mongoose');
+// import file models
+const Foodie = require('./models/food_models.js');
+// create app / server 
+const app = express();
 
-// membuat aplikasi / server 
-const app = express()
+// middleware, for read source json from client
+app.use = (express.json());
+
+// route post via body
+app.post('/food', async (req, res)=>{
+    try{
+        const Foodie = await Foodie.create(req.body);
+    } catch (error){
+        res.status(500).json({message: error.message});
+    }
+});
 
 // membuka server ke port
+// app.listen(3000, () => {
+//         console.log('Server is running on port 3000');
+// });
 
-// menerima permintaan get dari client
+// receive get request from client via URL
 app.get('/', (req, res) => {
     // res.send dan res.rediqrect gabisa digunakan barengan karena
     // hanya butuh satu respons dari server.
-    res.send("Heloo guys aku devi");
+    res.send("Heloo guys i'm devi");
     // res.redirect("https://media.istockphoto.com/id/1443562748/id/foto/kucing-jahe-lucu.jpg?s=612x612&w=0&k=20&c=g--RaRzUgk83osyJGx88ZtoChTk3VzP8n86EQHvc7Ko=");
 });
 
